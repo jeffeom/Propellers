35d812fd27cb1ff923d4f392d6ce497cba85c6d3
Jeff Eom <jeffeom@gmail.com>
2017-10-31 00:04:42 -0700

    found out that flashing only happen in iOS11

--- Propellers/Controllers/MessengerViewController.swift
+++ Propellers/Controllers/MessengerViewController.swift
@@ -26,7 +26,7 @@ class MessengerViewController: NMessengerViewController {
     super.viewDidLoad()
     currentUID = NetworkingService.shared.currentUID
     appearance()
-//    fetchMessages()
+    fetchMessages()
     fetchMessagesByChildAdded()
   }
   
@@ -48,6 +48,7 @@ extension MessengerViewController {
     self.messengerView.addMessages(self.messageGroups!, scrollsToMessage: false)
     self.messengerView.scrollToLastMessage(animated: false)
     self.lastMessageGroup = self.messageGroups?.last
+    automaticallyAdjustsScrollViewInsets = false
   }
   
   func fetchMessages() {
@@ -77,21 +78,19 @@ extension MessengerViewController {
       }
     }
   }
-
+  
   func fetchMessagesByChildAdded() {
     NetworkingService.shared.fetchMessagesByChildAdded(roomID: "abcd") { (message) in
-//      if !self.firstLoad {
-        if message.senderID != self.currentUID {
-          let textContent = TextContentNode(textMessageString: message.text!, currentViewController: self, bubbleConfiguration: self.sharedBubbleConfiguration)
-          let newMessage = MessageNode(content: textContent)
-          newMessage.cellPadding = self.messagePadding
-          newMessage.currentViewController = self
-          self.postText(newMessage, isIncomingMessage: true)
-        }
-//      }
+      if message.senderID != self.currentUID {
+        let textContent = TextContentNode(textMessageString: message.text!, currentViewController: self, bubbleConfiguration: self.sharedBubbleConfiguration)
+        let newMessage = MessageNode(content: textContent)
+        newMessage.cellPadding = self.messagePadding
+        newMessage.currentViewController = self
+        self.postText(newMessage, isIncomingMessage: true)
+      }
     }
   }
-
+  
   func fetchMessage(withText text: String, isIncomingMessage: Bool, lastMessage: Bool) {
     let textContent = TextContentNode(textMessageString: text, currentViewController: self, bubbleConfiguration: self.sharedBubbleConfiguration)
     let newMessage = MessageNode(content: textContent)
@@ -99,7 +98,6 @@ extension MessengerViewController {
     newMessage.currentViewController = self
     if messageGroups?.last == nil || messageGroups?.last?.isIncomingMessage == !isIncomingMessage {
       let newMessageGroup = self.createMessageGroup()
-      //add avatar if incoming message
       if isIncomingMessage {
         newMessageGroup.avatarNode = self.createAvatar()
       }
@@ -109,11 +107,6 @@ extension MessengerViewController {
     } else {
       messageGroups?.last?.addMessageToGroup(newMessage, completion: nil)
     }
-//    if lastMessage {
-//      messengerView.addMessage(newMessage, scrollsToMessage: false)
-//      messengerView.scrollToLastMessage(animated: false)
-//      lastMessageGroup = messageGroups?.last
-//    }
   }
 }
 
@@ -121,7 +114,6 @@ extension MessengerViewController {
 extension MessengerViewController {
   func appearance() {
     placeholderFix()
-    createTimeStamp()
     automaticallyAdjustsScrollViewInsets = false
   }
   
@@ -130,11 +122,10 @@ extension MessengerViewController {
     inputView.inputTextViewPlaceholder = "Type your message here ..."
   }
   
-  func createTimeStamp() {
-    //    let date = Date(milliseconds: selectedRoom?.timeUid2)
+  func createTimeStamp(messageDate dateInInt: Int64) {
+    guard let messageDate = Date(milliseconds: dateInInt) else { return }
     let currentDate = Date()
-    // [REFACTOR]
-    let dateDiff = Calendar.current.dateComponents([.day], from: currentDate, to: currentDate)
+    let dateDiff = Calendar.current.dateComponents([.day], from: messageDate, to: currentDate)
     if dateDiff.day == 0 {
       messageTimestamp.messageSentText = currentDate.toString(dateFormat: "h:mm a")
       self.addMessageToMessenger(messageTimestamp)
@@ -160,7 +151,6 @@ extension MessengerViewController {
       self.lastMessageGroup!.isIncomingMessage = isIncomingMessage
       self.messengerView.addMessageToMessageGroup(message, messageGroup: self.lastMessageGroup!, scrollsToLastMessage: false)
       self.messengerView.addMessage(self.lastMessageGroup!, scrollsToMessage: true, withAnimation: isIncomingMessage ? .left : .right)
-      
     } else {
       self.messengerView.addMessageToMessageGroup(message, messageGroup: self.lastMessageGroup!, scrollsToLastMessage: true)
     }
@@ -173,9 +163,9 @@ extension MessengerViewController {
     return newMessageGroup
   }
   
-  fileprivate func createAvatar()->ASImageNode {
+  fileprivate func createAvatar() -> ASImageNode {
     let avatar = ASImageNode()
-    avatar.image = UIImage(named: "nAvatar")
+    avatar.image = #imageLiteral(resourceName: "userPlaceHolder")
     avatar.backgroundColor = UIColor.lightGray
     avatar.style.preferredSize = CGSize(width: 20, height: 20)
     avatar.layer.cornerRadius = 10
